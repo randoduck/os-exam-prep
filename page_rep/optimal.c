bd2a574008390975b99885319c8caf0ddca3273d
Isha Telikicherla <isha.t.10c.1254@gmail.com>
2026-04-24 08:41:39 +0530

    Update optimal.c

--- page_rep/optimal.c
+++ page_rep/optimal.c
@@ -1,4 +1,43 @@
-  // if full → find optimal
+#include <stdio.h>
+
+int main() {
+    int n, f;
+    scanf("%d", &n);
+
+    int pages[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &pages[i]);
+
+    scanf("%d", &f);
+
+    int frame[f], faults = 0;
+
+    for(int i = 0; i < f; i++)
+        frame[i] = -1;
+
+    for(int i = 0; i < n; i++) {
+        int found = 0;
+
+        // check HIT
+        for(int j = 0; j < f; j++) {
+            if(frame[j] == pages[i]) {
+                found = 1;
+                break;
+            }
+        }
+
+        if(!found) {
+            int pos = -1;
+
+            // empty frame
+            for(int j = 0; j < f; j++) {
+                if(frame[j] == -1) {
+                    pos = j;
+                    break;
+                }
+            }
+
+            // if full → find optimal
             if(pos == -1) {
                 int farthest = -1;
 
@@ -18,3 +57,13 @@
                         farthest = k;
                         pos = j;
                     }
+                }
+            }
+
+            frame[pos] = pages[i];
+            faults++;
+        }
+    }
+
+    printf("Page Faults = %d\n", faults);
+}
